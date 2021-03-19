82db742de2cc79b1f2b9b6611f1bd452995f3819
timDINOS <tchoi720@gmail.com>
2021-03-18 22:35:48 -0700

    post user input on screen

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECT_MONGO_URL = 'mongodb+srv://tchoi123:Nbafinals123@cluster0.ea83s.mongodb.net/myFirstDatabase';
+const CONNECT_MONGO_URL = 'mongodb+srv://tchoi123:Nbafinals123@cluster0.ea83s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECT_MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
